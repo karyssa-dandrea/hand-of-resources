cd86519e7a4d5f6fe4d17f4ec53c25e8315ff346
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-17 11:32:10 -0700

    created routes and post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Avatar = require('../lib/models/Avatar');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,4 +12,16 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+});
+
+it('creates an avatar character', async () => {
+  const expected = {
+    name: 'Toph',
+    abilities: 'earthbending',
+  };
+  const res = await (
+    await request(app).post('/api/v1/avatar')
+  ).setEncoding(expected);
+
+  expect(res.body).toEqual({ id: expect.any(String), ...expected });
 });
