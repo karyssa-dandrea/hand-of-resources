ffe4ccbd8a8d61455226c380d7848c6f187f1b74
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-18 13:23:17 -0700

    findById test passing

--- __tests__/sushi.test.js
+++ __tests__/sushi.test.js
@@ -29,4 +29,11 @@ describe('hand-of-resources routes', () => {
 
     expect(res.body).toEqual(expected);
   });
+
+  it('gets sushi by id', async () => {
+    const expected = await Sushi.findById(1);
+    const res = await request(app).get(`/api/v1/sushi/${expected.id}`);
+
+    expect(res.body).toEqual({ ...expected });
+  });
 });
