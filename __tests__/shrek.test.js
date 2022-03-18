8cf931482f6bf3b36887aacf59b710d70ab05b52
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-18 15:46:57 -0700

    getAll test passing got shrek

--- __tests__/shrek.test.js
+++ __tests__/shrek.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Shrek = require('../lib/models/Shrek');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -20,5 +21,12 @@ describe('alchemy-app routes', () => {
     const res = await request(app).post('/api/v1/shrek').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets list of shrek characters', async () => {
+    const expected = await Shrek.findAll();
+    const res = await request(app).get('/api/v1/shrek');
+
+    expect(res.body).toEqual(expected);
   });
 });
