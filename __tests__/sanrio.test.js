2417d568b09b43a0d5acccdd7bc8d605eb6b64ef
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-18 20:53:32 -0700

    findAll test passing in sanrio

--- __tests__/sanrio.test.js
+++ __tests__/sanrio.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Sanrio = require('../lib/models/Sanrio');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -20,5 +21,12 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/sanrio').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets list of characters', async () => {
+    const expected = await Sanrio.findAll();
+    const res = await request(app).get('/api/v1/sanrio');
+
+    expect(res.body).toEqual(expected);
   });
 });
